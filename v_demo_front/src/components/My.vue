33170d9d5a19dc885937815f6ea0efe98dd88b9b
S-rookie <492258629@qq.com>
2021-04-28 17:41:45 +0800

    My.vue

---
+++ v_demo_front/src/components/My.vue
@@ -0,0 +1,35 @@
+<template>
+     <el-table
+    :data="tableData"
+    stripe
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="文件名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="大小">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="类型">
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            tableData: [],
+        }
+    }
+}
+</script>
